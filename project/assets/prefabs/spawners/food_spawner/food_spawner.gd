984aba99480d586dec4e3cf1391d6e9a1a657bfa
LordAsterius <loic.mathiot70@gmail.com>
2021-10-02 15:50:28 +0200

    Food spawner can now spawn food. Food can now be static

--- project/assets/prefabs/spawners/food_spawner/food_spawner.gd
+++ project/assets/prefabs/spawners/food_spawner/food_spawner.gd
@@ -1,5 +1,22 @@
 extends Interactable
 
+class_name FoodSpawner
+
+
+export(PackedScene) var food_to_spawn: PackedScene
+
+export(NodePath) var food_anchor_path
+onready var food_anchor := get_node(food_anchor_path)
+
+
+var food: Food
+
+
+func _ready() -> void:
+    food = food_to_spawn.instance() as Food
+    food_anchor.add_child(food)
+    food.make_food_static()
+    display_text = "Press E to get " + food.food_name.to_lower()
 
 func interact(instigator):
     print("plop")

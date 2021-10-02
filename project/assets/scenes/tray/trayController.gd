0066161429cadae8c1d1f1ad55c9e772213d9acb
SIL3nCe <tom.pradal@aliceadsl.fr>
2021-10-02 02:19:12 +0200

    Added tray max rotation

--- project/assets/scenes/tray/trayController.gd
+++ project/assets/scenes/tray/trayController.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
 export(float) var rotation_speed = 1.0
+export(float) var max_rotation_angle = 45.0
 var rot_angle = 0.0
 
 # Called when the node enters the scene tree for the first time.
@@ -11,9 +12,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	rot_angle = 0.0
-	if Input.is_action_pressed("tray_left"):
+	var current_angle = rad2deg(get_transform().basis.get_euler().z)
+	
+	if Input.is_action_pressed("tray_left") and current_angle < max_rotation_angle:
 		rot_angle = rotation_speed * delta
-	elif Input.is_action_pressed("tray_right"):
+	elif Input.is_action_pressed("tray_right") and current_angle > -max_rotation_angle:
 		rot_angle = -rotation_speed * delta
 	
 

c11a65bf7ddc84eb61451ae7e7eb89f1e56986e7
SIL3nCe <tom.pradal@aliceadsl.fr>
2021-10-02 23:51:00 +0200

    Handled rotation for food on tray, wip

--- project/assets/prefabs/objects/food/food.gd
+++ project/assets/prefabs/objects/food/food.gd
@@ -13,5 +13,28 @@ export(float) var food_reward := 0.0
 func make_food_static() -> void:
     rigid_body.set_mode(RigidBody.MODE_STATIC)
 
-func add_player_velocity(velocity: Vector3):
-    rigid_body.linear_velocity += velocity * 0.95
+func apply_player_velocity(velocity: Vector3) -> void:
+    rigid_body.linear_velocity += velocity * 1.0
+    
+func apply_player_rotation(rotation: float, player_location: Vector3) -> void:        
+    var food_location = rigid_body.global_transform.origin
+    rotation *= -1.0
+        
+    var x = food_location.x
+    var z = food_location.z
+
+    x -= player_location.x
+    z -= player_location.z
+
+    var xnew = x * cos(rotation) - z * sin(rotation)
+    var znew = x * sin(rotation) + z * cos(rotation)
+
+    x = xnew + player_location.x
+    z = znew + player_location.z
+
+    food_location.x = x
+    food_location.z = z
+    
+    #print(String(food_location) + "\n")
+    
+    rigid_body.global_transform.origin = food_location

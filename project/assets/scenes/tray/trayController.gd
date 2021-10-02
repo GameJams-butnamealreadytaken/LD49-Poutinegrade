dec5d3a0351bc52bedc6cc0622aa88ad5c30e4d9
SIL3nCe <tom.pradal@aliceadsl.fr>
2021-10-02 02:11:08 +0200

    Added tray behavior

--- project/assets/scenes/tray/trayController.gd
+++ project/assets/scenes/tray/trayController.gd
@@ -1,10 +1,7 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export(float) var rotation_speed = 1.0
+var rot_angle = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,5 +9,13 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	rot_angle = 0.0
+	if Input.is_action_pressed("tray_left"):
+		rot_angle = rotation_speed * delta
+	elif Input.is_action_pressed("tray_right"):
+		rot_angle = -rotation_speed * delta
+	
+
+func _physics_process(_delta):
+	rotate_z(rot_angle)

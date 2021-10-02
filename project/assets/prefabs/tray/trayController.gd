a01ddca9b93702a8027377b2e2e96db1d17d4cf2
SIL3nCe <tom.pradal@aliceadsl.fr>
2021-10-02 12:19:08 +0200

    Added tray reset + dyn/kine swap

--- project/assets/prefabs/tray/trayController.gd
+++ project/assets/prefabs/tray/trayController.gd
@@ -1,31 +1,50 @@
-extends Spatial
+extends RigidBody
 
 export(float) var rotation_speed = 1.0
+export(float) var rotation_max = 45.0
 
 var rot_angle_z = 0.0
 var rot_angle_x = 0.0
 
+var current_scale
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
-
+    current_scale = transform.basis.get_scale()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     rot_angle_x = 0.0
     rot_angle_z = 0.0
-        
-    if Input.is_action_pressed("tray_front"):
-        rot_angle_x = -rotation_speed * delta
-    elif Input.is_action_pressed("tray_back"):
-        rot_angle_x = rotation_speed * delta
-        
-    if Input.is_action_pressed("tray_left"):
-        rot_angle_z = rotation_speed * delta
-    elif Input.is_action_pressed("tray_right"):
-        rot_angle_z = -rotation_speed * delta
     
+    if mode == MODE_KINEMATIC:
+        var rotation_x = abs(rad2deg(get_transform().basis.get_euler().x))
+        var rotation_z = abs(rad2deg(get_transform().basis.get_euler().z))
+        
+        if rotation_x > rotation_max or rotation_z > rotation_max:
+            mode = MODE_RIGID
+    
+        if Input.is_action_pressed("tray_front"):
+            rot_angle_x = -rotation_speed * delta
+        elif Input.is_action_pressed("tray_back"):
+            rot_angle_x = rotation_speed * delta
+            
+        if Input.is_action_pressed("tray_left"):
+            rot_angle_z = rotation_speed * delta
+        elif Input.is_action_pressed("tray_right"):
+            rot_angle_z = -rotation_speed * delta
+            
+    if Input.is_action_pressed("tray_reset"):
+        resetTray()
+    
+func resetTray():
+    mode = MODE_KINEMATIC
+    
+    transform.basis = Basis()
+    transform = transform.basis.scaled(current_scale)
 
 func _physics_process(_delta):
-    rotate_x(rot_angle_x)
-    rotate_z(rot_angle_z)
+    if rot_angle_x != 0.0:
+        rotate_x(rot_angle_x)
+    if rot_angle_z != 0.0:
+        rotate_z(rot_angle_z)

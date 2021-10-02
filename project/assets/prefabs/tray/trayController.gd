9e3bc5be315fa5b9bfe5315af4b472565d9f0a34
SIL3nCe <tom.pradal@aliceadsl.fr>
2021-10-02 10:17:59 +0200

    Updated tray inputs + x rotation + new mesh

--- project/assets/prefabs/tray/trayController.gd
+++ project/assets/prefabs/tray/trayController.gd
@@ -2,7 +2,8 @@ extends Spatial
 
 export(float) var rotation_speed = 1.0
 export(float) var max_rotation_angle = 45.0
-var rot_angle = 0.0
+var rot_angle_z = 0.0
+var rot_angle_x = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,14 +12,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rot_angle = 0.0
-	var current_angle = rad2deg(get_transform().basis.get_euler().z)
+	rot_angle_z = 0.0
+	rot_angle_x = 0.0
 	
-	if Input.is_action_pressed("tray_left") and current_angle < max_rotation_angle:
-		rot_angle = rotation_speed * delta
-	elif Input.is_action_pressed("tray_right") and current_angle > -max_rotation_angle:
-		rot_angle = -rotation_speed * delta
+	var current_angle_z = rad2deg(get_transform().basis.get_euler().z)
+	var current_angle_x = rad2deg(get_transform().basis.get_euler().x)
 	
+	if Input.is_action_pressed("tray_left") and current_angle_z < max_rotation_angle:
+		rot_angle_z = rotation_speed * delta
+	elif Input.is_action_pressed("tray_right") and current_angle_z > -max_rotation_angle:
+		rot_angle_z = -rotation_speed * delta
+		
+	if Input.is_action_pressed("tray_front") and current_angle_x < max_rotation_angle:
+		rot_angle_x = rotation_speed * delta
+	elif Input.is_action_pressed("tray_back") and current_angle_x > -max_rotation_angle:
+		rot_angle_x = -rotation_speed * delta
 
 func _physics_process(_delta):
-	rotate_z(rot_angle)
+	rotate_z(rot_angle_z)
+	rotate_x(rot_angle_x)
